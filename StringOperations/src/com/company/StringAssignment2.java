05b131e21c7355d0b6f94028177a40a7cceed988
umbrayaerze <akhundovaee@gmail.com>
2021-02-15 18:49:04 +0200

    upd classes

--- StringOperations/src/com/company/StringAssignment2.java
+++ StringOperations/src/com/company/StringAssignment2.java
@@ -4,6 +4,8 @@ public class StringAssignment2 {
 
 
     public static void main(String[] args) {
+        String myString = "Climb mountains not so the world can see you, but so you can see the world";
         //
+
     }
 }
